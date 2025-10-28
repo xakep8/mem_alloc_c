9387884178a7c77f367e60424da6c6f6c7bb68cf
Kunal Dubey <xakep8@protonmail.com>
2025-10-28 16:13:37 +0530

    refactor: reorganize memory allocation code and improve structure definitions

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,2 +1,56 @@
 #include <stdio.h>
-#include 
+#include <unistd.h>
+// #include <sys/mman.h>
+
+typedef struct{
+	unsigned int block_size : 8;
+	unsigned int free : 1;
+} size_free;
+
+/*
+*/
+typedef struct{
+	size_free info;
+	char data[0];
+	struct mem_block* next;
+} mem_block;
+
+/*
+I will use this to keep track of the memory segments allocated.
+This will help me defragment the memory later on.
+also this will help me to find free blocks of memory when needed.
+*/
+struct mem_block* head = NULL;
+
+void* alloc(size_t size){
+	mem_block* ptr = (mem_block*)sbrk(size + sizeof(size_free));
+	if (!ptr) return NULL;
+
+	mem_block* block = (mem_block*)ptr;
+	block->info.block_size = size;
+	block->info.free = 1;
+	block->next = NULL;
+	return block;
+}
+
+void dealloc(void* p){
+	if (!p) return;
+	mem_block* block = (mem_block*)((char*)p - sizeof(size_free));
+	block->info.free = 1;
+}
+
+int main(){
+	int* p = (int*)alloc(4 * sizeof(int));
+	if (!p) {
+		fprintf(stderr, "Memory allocation failed\n");
+		return 1;
+	}
+	for (int i = 0; i < 4; i++) {
+		p[i] = i * 10;
+	}
+	for (int i = 0; i < 4; i++) {
+		printf("%d %p\n", p[i], &p[i]);
+	}
+	printf("\n");
+	return 0;
+}
