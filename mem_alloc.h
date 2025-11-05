8d79a3cc4ddb03ef0853320a826b5b40060eeaa4
Kunal Dubey <xakep8@protonmail.com>
2025-11-05 10:32:18 +0530

    refactor: enhance memory management by improving block splitting and coalescing logic

--- mem_alloc.h
+++ mem_alloc.h
@@ -39,6 +39,14 @@ typedef struct mem_block
 } mem_block;
 
 /*
+I will use this to keep track of the memory segments allocated.
+This will help me defragment the memory later on.
+also this will help me to find free blocks of memory when needed.
+*/
+struct mem_block *head = NULL;
+struct mem_block *tail = NULL;
+
+/*
 I'll be using memory pool of 10 MB initially.
 
 I'll be using this function to initialize the
@@ -54,14 +62,26 @@ void init_allocate_memory_pool()
 		throw("Initial memory pool allocation failed");
 	}
 	current_brk = memory_pool;
+	tail = NULL;
+	head = NULL;
 }
 
-/*
-I will use this to keep track of the memory segments allocated.
-This will help me defragment the memory later on.
-also this will help me to find free blocks of memory when needed.
-*/
-struct mem_block *head = NULL;
+static void maybe_split_block(mem_block *block, size_t size)
+{
+	size_t total_size = size + sizeof(mem_block);
+	if (block->info.block_size >= total_size + sizeof(mem_block) + 8) // minimum split size
+	{
+		mem_block *new_block = (mem_block *)((char *)block + total_size);
+		new_block->info.block_size = block->info.block_size - total_size;
+		new_block->info.free = 1;
+		new_block->next = block->next;
+
+		block->info.block_size = total_size;
+		block->next = new_block;
+		if (tail == block)
+			tail = new_block;
+	}
+}
 
 /*
 seems to be working fine for now.
@@ -79,6 +99,17 @@ void *alloc(size_t size)
 	}
 	size_t total_size = size + sizeof(mem_block);
 	size_t aligned_size = (total_size + 7) & ~7; // align to 8 bytes
+
+	for (mem_block *cur = head; cur != NULL; cur = cur->next)
+	{
+		if (cur->info.free && cur->info.block_size >= aligned_size)
+		{
+			cur->info.free = 0;
+			maybe_split_block(cur, size);
+			return cur->data;
+		}
+	}
+
 	// size to be allocate + current break pointer should not exceed memory pool
 	if ((char *)current_brk + aligned_size > (char *)memory_pool + size_of_memory_pool)
 	{
@@ -88,10 +119,43 @@ void *alloc(size_t size)
 	ptr->info.block_size = aligned_size;
 	ptr->info.free = 0;
 	ptr->next = head;
-	head = ptr;
+	if (!head)
+	{
+		head = ptr;
+		tail = ptr;
+	}
+	else
+	{
+		tail->next = ptr;
+		tail = ptr;
+	}
 
 	current_brk = (char *)current_brk + aligned_size;
 	return ptr->data;
+}
+
+// merge adjacent free blocks
+void coalesce_free_blocks()
+{
+	mem_block *cur = head;
+	while (cur && cur->next)
+	{
+		mem_block *nxt = cur->next;
+		if (cur->info.free && nxt->info.free &&
+			(char *)cur + cur->info.block_size == (char *)nxt)
+		{
+			// merge nxt into cur
+			cur->info.block_size += nxt->info.block_size;
+			cur->next = nxt->next;
+			if (tail == nxt)
+				tail = cur;
+			// stay on cur to check further merges
+		}
+		else
+		{
+			cur = cur->next;
+		}
+	}
 }
 
 void cleanup_memory_pool()
@@ -102,6 +166,7 @@ void cleanup_memory_pool()
 		memory_pool = NULL;
 		current_brk = NULL;
 		head = NULL;
+		tail = NULL;
 	}
 }
 
@@ -113,9 +178,5 @@ void dealloc(void *p)
 	size_t payload = block->info.block_size - sizeof(mem_block);
 	memset(block->data, 0, payload);
 	block->info.free = 1;
-}
-
-// merge adjacent free blocks
-void coalesce_free_blocks()
-{
+	coalesce_free_blocks();
 }
